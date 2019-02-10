4e268e3084d160eba6a608c13c241b9ba252f434
Joe Osborne <3324453+ettmetal@users.noreply.github.com>
2019-02-10 16:16:30 +0100

    Add some handy Editor wrappers

---
+++ Assets/Translations/Editor/EditorGUITools.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+namespace Ettmetal.Translation.Editor {
+    public static class EditorGUITools {
+        
+        public static void DoHorizontal(Action horizontalGUI) {
+            EditorGUILayout.BeginHorizontal();
+            horizontalGUI();
+            EditorGUILayout.EndHorizontal();
+        }
+
+        public static void DoVertical(Action verticalGUI) {
+            EditorGUILayout.BeginVertical();
+            verticalGUI();
+            EditorGUILayout.EndVertical();
+        }
+
+        public static Vector2 DoScroll(Action scrolledGUI, Vector2	scrollPosition) {
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+            scrolledGUI();
+            EditorGUILayout.EndScrollView();
+            return scrollPosition;
+        }
+    }
+}
