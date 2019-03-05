3e5dbad0cf7642f5fc8d3689928d7bd2a9f02a3e
Joe Osborne <3324453+ettmetal@users.noreply.github.com>
2019-03-05 17:37:43 +0100

    Add LocaleSetter script

---
+++ Assets/Translations/LocaleSetter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Ettmetal.Translation {
+	// Script for hooking up e.g. UnityEvents to static Il8n ChangeLocale()
+    public class LocaleSetter : MonoBehaviour {
+		public void ChangeLocale(string newLocale) {
+			Il8n.ChangeLocale(newLocale);
+		}
+    }
+}
