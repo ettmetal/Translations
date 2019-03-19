ff78c2d9c6af67b264d1e00719954c0a744778f7
Joe Osborne <3324453+ettmetal@users.noreply.github.com>
2019-03-19 17:05:57 +0000

    Add script that will provide a locale select dropdown behaviour

---
+++ Assets/Translations/LocaleDropdown.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ettmetal.Translation {
+    public class LocaleDropdown : MonoBehaviour {
+
+		private Dictionary<int, string> localesByIndex;
+        // Start is called before the first frame update
+        void Start() {
+			
+        }
+
+		private void onDropdownChanged(int value) {
+			Il8n.ChangeLocale(localesByIndex[value]);
+		}
+    }
+}
