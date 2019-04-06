90e96187f2e1589101b341a001f51aeaf199fa9e
Joe Osborne <3324453+ettmetal@users.noreply.github.com>
2019-04-06 19:42:32 +0200

    Add LocaleDropdown editor

---
+++ Assets/Translations/Editor/LocaleDropdownEditor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.UI;
+using UnityEditor;
+using UnityEditor.UI;
+
+namespace Ettmetal.Translation.Editor {
+	[CustomEditor(typeof(LocaleDropdown), true)]
+	[CanEditMultipleObjects]
+	public class LocaleDropdownEditor : SelectableEditor {
+		private SerializedProperty template;
+		private SerializedProperty captionText;
+		private SerializedProperty captionImage;
+		private SerializedProperty itemText;
+		private SerializedProperty itemImage;
+		private SerializedProperty value;
+
+		protected override void OnEnable() {
+			base.OnEnable();
+			template = serializedObject.FindProperty("m_Template");
+			captionText = serializedObject.FindProperty("m_CaptionText");
+			captionImage = serializedObject.FindProperty("m_CaptionImage");
+			itemText = serializedObject.FindProperty("m_ItemText");
+			itemImage = serializedObject.FindProperty("m_ItemImage");
+			value = serializedObject.FindProperty("m_Value");
+		}
+
+		public override void OnInspectorGUI() {
+			base.OnInspectorGUI();
+			EditorGUILayout.Space();
+			serializedObject.Update();
+			EditorGUILayout.PropertyField(template);
+			EditorGUILayout.PropertyField(captionText);
+			EditorGUILayout.PropertyField(captionImage);
+			EditorGUILayout.PropertyField(itemText);
+			EditorGUILayout.PropertyField(itemImage);
+			EditorGUILayout.PropertyField(value);
+			serializedObject.ApplyModifiedProperties();
+		}
+	}
+}
