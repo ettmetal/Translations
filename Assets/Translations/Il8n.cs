cbff25fad2576539e5aaf01c0da02e8363ea9bc6
Joe Osborne <3324453+ettmetal@users.noreply.github.com>
2019-04-06 19:31:07 +0200

    Make locale dropdown functional

--- Assets/Translations/Il8n.cs
+++ Assets/Translations/Il8n.cs
@@ -2,74 +2,84 @@
 using UnityEngine;
 
 namespace Ettmetal.Translation {
-    // Provides translation services.
-    // Includes both handling text localisation and preferences.
-    public static class Il8n {
-        private static TranslationSettings settings;
-        private static LocaleData defaultLocale, activeLocale;
-        public static event Action OnLocaleChanged;
+	// Provides translation services.
+	// Includes both handling text localisation and preferences.
+	public static class Il8n {
+		private static TranslationSettings settings;
+		private static LocaleData defaultLocale, activeLocale;
+		public static event Action OnLocaleChanged;
+		private static string[] availableLocales;
 
-        static Il8n() {
-            settings = Resources.Load<TranslationSettings>(Strings.SettingsPath);
-            if(!PlayerPrefs.HasKey(Strings.LocalePref)) {
-                setLocale(settings.DefaultLocale);
-            }
-            defaultLocale = loadLocale(settings.DefaultLocale);
-            if(PlayerPrefs.GetString(Strings.LocalePref) == settings.DefaultLocale) {
-                activeLocale = defaultLocale;
-            }
-            else {
-                activeLocale = loadLocale(PlayerPrefs.GetString(Strings.LocalePref));
-            }
-        }
+		public static string[] AvailableLocales {
+			get {return availableLocales;}
+		}
+		static Il8n() {
+			settings = Resources.Load<TranslationSettings>(Strings.SettingsPath);
+			LocaleData[] locales = Resources.LoadAll<LocaleData>(settings.LocalesResourcePath);
+			availableLocales = new string[locales.Length];
+			for(int localeIndex = 0; localeIndex < locales.Length; localeIndex++){
+				availableLocales[localeIndex] = locales[localeIndex].Name;
+			}
+			if(!PlayerPrefs.HasKey(Strings.LocalePref)) {
+				setLocale(settings.DefaultLocale);
+			}
+			defaultLocale = loadLocale(settings.DefaultLocale);
+			if(PlayerPrefs.GetString(Strings.LocalePref) == settings.DefaultLocale) {
+				activeLocale = defaultLocale;
+			}
+			else {
+				activeLocale = loadLocale(PlayerPrefs.GetString(Strings.LocalePref));
+			}
+			Resources.UnloadUnusedAssets();
+		}
 
-        // Core internationalisation interface.
-        public static string __(string key) {
-            string translation = activeLocale[key].Value;
+		// Core internationalisation interface.
+		public static string __(string key) {
+			string translation = activeLocale[key].Value;
 
-            if(string.IsNullOrEmpty(translation)) {
-                Debug.LogFormat(Strings.FallbackToDefaultFormat, activeLocale.Name, key);
-                translation = defaultLocale[key].Value;
-            }
+			if(string.IsNullOrEmpty(translation)) {
+				Debug.LogFormat(Strings.FallbackToDefaultFormat, activeLocale.Name, key);
+				translation = defaultLocale[key].Value;
+			}
 
-            if(string.IsNullOrEmpty(translation)) {
-                Debug.LogWarningFormat(Strings.NoDefaultValueFormat, defaultLocale.Name, key);
-            }
+			if(string.IsNullOrEmpty(translation)) {
+				Debug.LogWarningFormat(Strings.NoDefaultValueFormat, defaultLocale.Name, key);
+			}
 
-            return translation;
-        }
+			return translation;
+		}
 
-        public static string __(string key, int count) {
-            string translation = activeLocale[key].Pluralised(count);
-            if(string.IsNullOrEmpty(translation)) {
-                Debug.LogFormat(Strings.FallbackToDefaultFormat, activeLocale.Name, key);
-                translation = defaultLocale[key].Pluralised(count);
-            }
-            if(string.IsNullOrEmpty(translation)) {
-                Debug.LogWarningFormat(Strings.NoDefaultValueFormat, defaultLocale.Name, key);
-            }
-            return translation;
-        }
+		public static string __(string key, int count) {
+			string translation = activeLocale[key].Pluralised(count);
+			if(string.IsNullOrEmpty(translation)) {
+				Debug.LogFormat(Strings.FallbackToDefaultFormat, activeLocale.Name, key);
+				translation = defaultLocale[key].Pluralised(count);
+			}
+			if(string.IsNullOrEmpty(translation)) {
+				Debug.LogWarningFormat(Strings.NoDefaultValueFormat, defaultLocale.Name, key);
+			}
+			return translation;
+		}
 
-        public static void ChangeLocale(string newLocale) {
-            if(PlayerPrefs.GetString(Strings.LocalePref) != newLocale) {
-                setLocale(newLocale);
-                activeLocale = loadLocale(newLocale);
-                OnLocaleChanged();
-            }
-        }
+		public static void ChangeLocale(string newLocale) {
+			if(PlayerPrefs.GetString(Strings.LocalePref) != newLocale) {
+				setLocale(newLocale);
+				activeLocale = loadLocale(newLocale);
+				OnLocaleChanged();
+			}
+		}
 
-        private static void setLocale(string newLocale) {
-            PlayerPrefs.SetString(Strings.LocalePref, newLocale);
-            PlayerPrefs.Save();
-        }
+		private static void setLocale(string newLocale) {
+			PlayerPrefs.SetString(Strings.LocalePref, newLocale);
+			PlayerPrefs.Save();
+		}
 
-        private static LocaleData loadLocale(string locale) {
+		private static LocaleData loadLocale(string locale) {
 			LocaleData loadedLocale = Resources.Load<LocaleData>(settings.LocalesResourcePath + locale);
 			if(loadedLocale == null) {
 				throw new ArgumentOutOfRangeException(string.Format(Strings.LocaleNotFoundFormat, locale));
 			}
 			return loadedLocale;
-        }
-    }
+		}
+	}
 }
--- Assets/Translations/LocaleDropdown.cs
+++ Assets/Translations/LocaleDropdown.cs
@@ -1,17 +1,42 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Ettmetal.Translation {
-    public class LocaleDropdown : MonoBehaviour {
+	public class LocaleDropdown : Dropdown {
 
 		private Dictionary<int, string> localesByIndex;
-        // Start is called before the first frame update
-        void Start() {
-			
-        }
+		// Start is called before the first frame update
+		protected override void Start() {
+			base.Start();
+			localesByIndex = new Dictionary<int, string>();
+			string[] localeNames = Il8n.AvailableLocales;
+			for(int localeIndex = 0; localeIndex < localeNames.Length; localeIndex++) {
+				localesByIndex.Add(localeIndex, localeNames[localeIndex]);
+				options.Add(new OptionData(localeNames[localeIndex]));
+			}
+			onValueChanged.AddListener(onDropdownChanged);
+		}
+
+		protected override void OnEnable() {
+			base.OnEnable();
+			Il8n.OnLocaleChanged += updateOptions;
+		}
+
+		protected override void OnDisable() {
+			base.OnDisable();
+			Il8n.OnLocaleChanged -= updateOptions;
+		}
 
 		private void onDropdownChanged(int value) {
 			Il8n.ChangeLocale(localesByIndex[value]);
 		}
-    }
+
+		private void updateOptions() {
+			ClearOptions();
+			foreach(string localeName in localesByIndex.Values) {
+				options.Add(new OptionData(Il8n.__(localeName)));
+			}
+		}
+	}
 }
