ff15280ce8a91492277ac3fd92abd7ca146a71ed
Joe Osborne <3324453+ettmetal@users.noreply.github.com>
2019-05-04 20:15:48 +0200

    Add a method for LocalizedText to be updated manually
    Needed in circumstances where tokens might change

--- Assets/Translations/LocalizedText.cs
+++ Assets/Translations/LocalizedText.cs
@@ -2,28 +2,32 @@ using UnityEngine;
 using UnityEngine.UI;
 
 namespace Ettmetal.Translation {
-    [AddComponentMenu("UI/Localized Text")]
-    public class LocalizedText : Text {
-        [SerializeField]
-        private string localizationKey;
+	[AddComponentMenu("UI/Localized Text")]
+	public class LocalizedText : Text {
+		[SerializeField]
+		private string localizationKey = null;
 
-        protected override void Awake() {
-            base.Awake();
-            updateLocalizedText();
-        }
+		public void UpdateText() {
+			updateLocalizedText();
+		}
 
-        protected override void OnEnable() {
-            base.OnEnable();
-            Il8n.OnLocaleChanged += updateLocalizedText;
-        }
+		protected override void Awake() {
+			base.Awake();
+			updateLocalizedText();
+		}
 
-        protected override void OnDisable() {
-            base.OnDisable();
-            Il8n.OnLocaleChanged -= updateLocalizedText;
-        }
+		protected override void OnEnable() {
+			base.OnEnable();
+			Il8n.OnLocaleChanged += updateLocalizedText;
+		}
 
-        private void updateLocalizedText() {
-            text = Il8n.__(localizationKey);
-        }
-    }
+		protected override void OnDisable() {
+			base.OnDisable();
+			Il8n.OnLocaleChanged -= updateLocalizedText;
+		}
+
+		private void updateLocalizedText() {
+			text = Il8n.__(localizationKey);
+		}
+	}
 }
